09e2c2c74444c58c65bd422b139ab3ca6ced1861
rysdev <31880107+rysdev@users.noreply.github.com>
2018-09-11 18:07:24 -0700

    Add files via upload

---
+++ LatestContactModel/JPLatestContact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JobPlacementDashboard.Models
+{
+    public class JPLatestContact
+    {
+        [Key]
+        public Guid JPLatestContactId { get; set; }
+        [DisplayName("Latest Contact Date")]
+        public DateTime JPLatestContactDate { get; set; }
+        public string ApplicationUserId { get; set; }
+
+        public int CalculateLastContactDate
+        {
+            get
+            {
+                int days = (DateTime.Now - JPLatestContactDate).Days;
+                return (days);
+            }
+        }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
